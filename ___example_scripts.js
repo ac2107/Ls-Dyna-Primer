1fb5ad06a5ea32b9ce2e2361f3ee3f5f7f0b7e61
Anqi Chen <78877022+ac2107@users.noreply.github.com>
2023-08-08 09:03:43 +0100

    Update ___example_scripts.js

--- ___example_scripts.js
+++ ___example_scripts.js
@@ -1,83 +1,83 @@
 //==============================================================================
-var beam_references = {
+// var beam_references = {
 
-    ROOF: {
-        bids: [
+//     ROOF: {
+//         bids: [
 
-        ],
-        section: 'roof connection beam elements',
-        bsid: 101,
-    },
+//         ],
+//         section: 'roof connection beam elements',
+//         bsid: 101,
+//     },
 
-	LEVEL_2: {
-        bids: [
+// 	LEVEL_2: {
+//         bids: [
 			
-        ],
-        section: 'level 2 connection beam elements',
-        bsid: 102,
-    },
+//         ],
+//         section: 'level 2 connection beam elements',
+//         bsid: 102,
+//     },
 
-	LEVEL_1: {
-        bids: [
-
-			
-        ],
-        section: 'level 1 connection beam elements',
-        bsid: 103,
-    },
-
-	TRUSS: {
-        bids: [
-
-        ],
-        section: 'cantilever truss connection beam elements',
-        bsid: 104,
-    },
-
-	BRACING: {
-        bids: [
+// 	LEVEL_1: {
+//         bids: [
 
 			
-        ],
-        section: 'bracing connection beam elements',
-        bsid: 105,
-    },
+//         ],
+//         section: 'level 1 connection beam elements',
+//         bsid: 103,
+//     },
 
-	CORRIDOR: {
-        bids: [
+// 	TRUSS: {
+//         bids: [
+
+//         ],
+//         section: 'cantilever truss connection beam elements',
+//         bsid: 104,
+//     },
+
+// 	BRACING: {
+//         bids: [
+
+			
+//         ],
+//         section: 'bracing connection beam elements',
+//         bsid: 105,
+//     },
+
+// 	CORRIDOR: {
+//         bids: [
 
 		
 			
-        ],
-        section: 'corridor connection beam elements',
-        bsid: 106,
-    },
+//         ],
+//         section: 'corridor connection beam elements',
+//         bsid: 106,
+//     },
 
-	COLUMNS: {
-        bids: [
+// 	COLUMNS: {
+//         bids: [
 					
-        ],
-        section: 'column base connection beam elements',
-        bsid: 107,
-	}
+//         ],
+//         section: 'column base connection beam elements',
+//         bsid: 107,
+// 	}
 
-};
+// };
 
-const bset_connection_all = new Set(m, 20001, Set.BEAM, 'all connection beam elements')
-var csv = new File(js_dir + "beam_sets_all.csv", File.WRITE);
-const kList = Object.keys(beam_references);
-for (var i = 0; i < kList.length; i++) {
-    var key = kList[i];
-    var bref = beam_references[key];
-    if (bref.bids.length > 0) {
-        var bset = new Set(m, bref.bsid, Set.BEAM, key + ' ' + bref.section);
-        for (var j = 0; j < bref.bids.length; j++) {
-            bset.Add(bref.bids[j]);
-            bset_connection_all.Add(bref.bids[j]);
-        }
-        csv.Writeln(key + ',' + bref.section + ',' + bref.bids);
-    }
-}
-csv.Close();
+// const bset_connection_all = new Set(m, 20001, Set.BEAM, 'all connection beam elements')
+// var csv = new File(js_dir + "beam_sets_all.csv", File.WRITE);
+// const kList = Object.keys(beam_references);
+// for (var i = 0; i < kList.length; i++) {
+//     var key = kList[i];
+//     var bref = beam_references[key];
+//     if (bref.bids.length > 0) {
+//         var bset = new Set(m, bref.bsid, Set.BEAM, key + ' ' + bref.section);
+//         for (var j = 0; j < bref.bids.length; j++) {
+//             bset.Add(bref.bids[j]);
+//             bset_connection_all.Add(bref.bids[j]);
+//         }
+//         csv.Writeln(key + ',' + bref.section + ',' + bref.bids);
+//     }
+// }
+// csv.Close();
 
 //==============================================================================
