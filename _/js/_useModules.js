eae7efd428119edb8f079ba543e073f8f507a9e9
Anqi Chen <78877022+ac2107@users.noreply.github.com>
2023-08-24 21:51:16 +0100

    Create _useModules.js

---
+++ _/js/_useModules.js
@@ -0,0 +1,24 @@
+// include all JavaScript to be used in Primer
+// Use("PrimerJS_functions.js");
+// Use("Ls_Dyna_functions.js");
+
+Use("defineBlastLoad.js");
+// Use("columnBlastFace.js");
+// Use('loadPanelViperBlast.js');
+
+Use("getNodesBetweenTwoNodes.js");
+Use("getBeamElementsByNodes.js");
+
+Use("lineMesh.js");
+Use('shellMesh.js');
+Use('solidMesh.js');
+
+Use('analysisControl.js');
+// Use("theBlueBookP363.js");
+Use('utility.js');
+Use('material.js');
+Use('orient.js');
+
+// Use("beamEndRelease.js");
+// Use('nodeTieNRBC.js');
+// Use('beamSectionUpdate.js');
